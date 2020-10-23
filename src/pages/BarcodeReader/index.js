a3009b376bd4910c3059afd9a83207f428e83073
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-23 14:19:42 -0300

    feat: add mask for barcode-scanner

--- src/pages/BarcodeReader/index.js
+++ src/pages/BarcodeReader/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import BarcodeMask from 'react-native-barcode-mask';
+
+import styles from './styles';
 
 export default function index() {
+	const [hasPermission, setHasPermission] = useState(null);
+	const [scanned, setScanned] = useState(false);
+
+	useEffect(() => {
+		(async () => {
+			const { status } = await BarCodeScanner.requestPermissionsAsync();
+			setHasPermission(status === 'granted');
+		})();
+	}, []);
+
+	const handleBarCodeScanned = ({ type, data }) => {
+		setScanned(true);
+		alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+	};
+
+	if (hasPermission === null) {
+		return <Text>Requesting for camera permission</Text>;
+	}
+	if (hasPermission === false) {
+		return <Text>No access to camera</Text>;
+	}
+
 	return (
-		<View>
-			<Text>This is the barcode reader</Text>
+		<View style={styles.container}>
+			<BarCodeScanner
+				onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+				style={StyleSheet.absoluteFillObject}
+			>
+				<BarcodeMask
+					lineAnimationDuration={1000}
+					animatedLineColor={'whitesmoke'}
+				/>
+			</BarCodeScanner>
+
+			{scanned && (
+				<Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+			)}
 		</View>
 	);
 }
---
+++ src/pages/BarcodeReader/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+
+	text: {
+		color: 'red',
+	},
+
+	barcodeScanner: {
+		width: '100%',
+		height: '100%',
+	},
+});

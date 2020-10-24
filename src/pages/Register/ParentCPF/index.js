70bf0a494763a6c9513bc4ff0e1d3591a9a6f146
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-24 17:48:04 -0300

    chore: add CEP and CPF information after parent consent

--- src/pages/Register/ParentCPF/index.js
+++ src/pages/Register/ParentCPF/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 export default function index() {
+	const [cpf, setCPF] = useState('');
+	const [cep, setCEP] = useState('');
+
+	const navigateToAddChildrenScreen = () => {
+		// Navigate
+		console.warn('add children');
+	};
+
 	return (
 		<View style={styles.container}>
-			<Text>Parent CPF!</Text>
+			<Text style={styles.text}>
+				Informe apenas mais alguns dados para que possamos melhorar nossas
+				políticas públicas :)
+			</Text>
+			<View style={styles.inputContainer}>
+				<Text style={styles.inputTitle}>CPF</Text>
+				<TextInput
+					keyboardType='numeric'
+					placeholder='15298865298'
+					style={styles.textInput}
+					value={cpf}
+					onChangeText={(text) => setCPF(text)}
+				></TextInput>
+			</View>
+
+			<View style={styles.inputContainer}>
+				<Text style={styles.inputTitle}>CEP (código postal)</Text>
+				<TextInput
+					keyboardType='numeric'
+					placeholder='29101558'
+					style={styles.textInput}
+					value={cep}
+					onChangeText={(text) => setCEP(text)}
+				></TextInput>
+			</View>
+
+			<TouchableOpacity
+				style={styles.yes}
+				onPress={navigateToAddChildrenScreen}
+			>
+				<View>
+					<Text style={styles.textTouchable}>Avançar</Text>
+				</View>
+			</TouchableOpacity>
 		</View>
 	);
 }
--- src/pages/Register/ParentCPF/styles.js
+++ src/pages/Register/ParentCPF/styles.js
@@ -8,6 +8,44 @@ export default StyleSheet.create({
 	},
 
 	text: {
-		color: 'red',
+		fontSize: 15,
+		fontWeight: 'bold',
+		width: 250,
+		marginBottom: 50,
+	},
+
+	inputContainer: {
+		justifyContent: 'center',
+		padding: 10,
+	},
+
+	inputTitle: {
+		alignSelf: 'flex-start',
+		padding: 3,
+		fontSize: 20,
+	},
+
+	textInput: {
+		height: 80,
+		width: 280,
+		borderRadius: 10,
+		borderColor: 'gray',
+		borderWidth: 1,
+		fontSize: 30,
+		paddingLeft: 12,
+	},
+
+	yes: {
+		justifyContent: 'center',
+		width: 120,
+		marginTop: 15,
+		borderColor: 'black',
+		borderWidth: 1,
+		borderRadius: 20,
+		padding: 10,
+	},
+
+	textTouchable: {
+		textAlign: 'center',
 	},
 });

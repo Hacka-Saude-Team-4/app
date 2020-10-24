a8da497a8836eebe4481f6f75b494dd7185da38b
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-24 14:52:38 -0300

    feat: create first form inside ParentDetails

--- src/pages/Register/ParentDetails/index.js
+++ src/pages/Register/ParentDetails/index.js
@@ -6,83 +6,131 @@ import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
-export default function index({ route, navigation }) {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+export default function index({ navigation }) {
+	const [name, setName] = useState('');
+	const [birthdate, setBirthdate] = useState('');
+	const [gender, setGender] = useState('');
+	const [relationship, setRelationship] = useState('');
+	const [buttonPressed, setButtonPressed] = useState(false);
 
-	const readData = async (key) => {
-		try {
-			const value = await AsyncStorage.getItem(key);
-			if (value !== null) {
-				console.warn(value);
-			}
-		} catch (e) {
-			// error reading value
+	const next = () => {
+		if (
+			name !== '' &&
+			birthdate !== '' &&
+			gender !== '' &&
+			relationship !== ''
+		) {
+			setButtonPressed(true);
+		} else {
+			alert('Por favor preencha todos os campos antes de continuar.');
 		}
-	};
-
-	const test = () => {
-		readData('accessToken');
 	};
 
 	return (
 		<View style={styles.container}>
-			<Text>Criar conta como responsável</Text>
-			<View style={styles.inputContainer}>
-				<Text style={styles.inputTitle}>Nome completo</Text>
-				<TextInput
-					autoCapitalize='none'
-					textContentType='emailAddress'
-					placeholder='João da Silva'
-					style={styles.textInput}
-					value={email}
-					onChangeText={(text) => setEmail(text)}
-				></TextInput>
-			</View>
+			{!buttonPressed && (
+				<View style={styles.container}>
+					<Text>Criar conta como responsável</Text>
+					<View style={styles.inputContainer}>
+						<Text style={styles.inputTitle}>Nome completo</Text>
+						<TextInput
+							autoCapitalize='none'
+							placeholder='João da Silva'
+							style={styles.textInput}
+							value={name}
+							onChangeText={(text) => setName(text)}
+						></TextInput>
+					</View>
+					<View style={styles.inputContainer}>
+						<Text style={styles.inputTitle}>Data de nascimento</Text>
+						<TextInput
+							autoCapitalize='none'
+							placeholder='24/11/1984'
+							style={styles.textInput}
+							value={birthdate}
+							onChangeText={(text) => setBirthdate(text)}
+						></TextInput>
+					</View>
+					<View style={styles.inputContainer}>
+						<Text style={styles.inputTitle}>Gênero</Text>
+						<TextInput
+							autoCapitalize='none'
+							placeholder='Homem'
+							style={styles.textInput}
+							value={gender}
+							onChangeText={(text) => setGender(text)}
+						></TextInput>
+					</View>
+					<View style={styles.inputContainer}>
+						<Text style={styles.inputTitle}>
+							Grau de parentesco com a(s) criança(s)
+						</Text>
+						<TextInput
+							autoCapitalize='none'
+							placeholder='Pai'
+							style={styles.textInput}
+							value={relationship}
+							onChangeText={(text) => setRelationship(text)}
+						></TextInput>
+					</View>
 
-			<View style={styles.inputContainer}>
-				<Text style={styles.inputTitle}>Data de nascimento</Text>
-				<TextInput
-					autoCapitalize='none'
-					placeholder='24/11/1984'
-					style={styles.textInput}
-					textContentType='password'
-					value={password}
-					onChangeText={(text) => setPassword(text)}
-				></TextInput>
-			</View>
-
-			<View style={styles.inputContainer}>
-				<Text style={styles.inputTitle}>Gênero</Text>
-				<TextInput
-					autoCapitalize='none'
-					placeholder='Homem'
-					style={styles.textInput}
-					textContentType='password'
-					value={password}
-					onChangeText={(text) => setPassword(text)}
-				></TextInput>
-			</View>
-
-			<View style={styles.inputContainer}>
-				<Text style={styles.inputTitle}>
-					Grau de parentesco com a(s) criança(s)
-				</Text>
-				<TextInput
-					autoCapitalize='none'
-					placeholder='Pai'
-					style={styles.textInput}
-					textContentType='password'
-					value={password}
-					onChangeText={(text) => setPassword(text)}
-				></TextInput>
-			</View>
-
-			<TouchableOpacity style={styles.sendBtn} onPress={test}>
-				<View>
-					<Text>Próximo</Text>
+					<TouchableOpacity style={styles.sendBtn} onPress={next}>
+						<View>
+							<Text>Próximo</Text>
+						</View>
+					</TouchableOpacity>
 				</View>
-			</TouchableOpacity>
+			)}
+
+			{buttonPressed && (
+				<View>
+					<Text>pressed</Text>
+				</View>
+			)}
+
+			{/* <View>
+				<View style={styles.inputContainer}>
+					<Text style={styles.inputTitle}>
+						Grau de parentesco com a(s) criança(s)
+					</Text>
+					<TextInput
+						autoCapitalize='none'
+						placeholder='Pai'
+						style={styles.textInput}
+						textContentType='password'
+						value={password}
+						onChangeText={(text) => setPassword(text)}
+					></TextInput>
+				</View>
+
+				<View style={styles.inputContainer}>
+					<Text style={styles.inputTitle}>
+						Grau de parentesco com a(s) criança(s)
+					</Text>
+					<TextInput
+						autoCapitalize='none'
+						placeholder='Pai'
+						style={styles.textInput}
+						textContentType='password'
+						value={password}
+						onChangeText={(text) => setPassword(text)}
+					></TextInput>
+				</View>
+
+				<View style={styles.inputContainer}>
+					<Text style={styles.inputTitle}>
+						Grau de parentesco com a(s) criança(s)
+					</Text>
+					<TextInput
+						autoCapitalize='none'
+						placeholder='Pai'
+						style={styles.textInput}
+						textContentType='password'
+						value={password}
+						onChangeText={(text) => setPassword(text)}
+					></TextInput>
+				</View>
+			</View> */}
 		</View>
 	);
 }

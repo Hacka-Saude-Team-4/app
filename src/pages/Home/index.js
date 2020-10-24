fa253b09c4c30c47f853c8cbefa6d75eca498da7
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-24 18:45:16 -0300

    feat: add tab-view for Home screen and routing based on user authentication state

---
+++ src/pages/Home/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, Dimensions } from 'react-native';
+// import styles from './styles';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import { Feather } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { SimpleLineIcons } from '@expo/vector-icons';
+
+import AddChildren from '../AddChildren';
+
+const FirstRoute = () => (
+	<View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+);
+
+const SecondRoute = () => (
+	<View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+);
+
+const getTabBarIcon = (props) => {
+	const { route } = props;
+
+	if (route.key === 'account') {
+		return <MaterialCommunityIcons name='account' size={24} color='white' />;
+	} else if (route.key === 'settings') {
+		return <Feather name='settings' size={24} color='white' />;
+	} else if (route.key === 'stats') {
+		return <SimpleLineIcons name='graph' size={24} color='white' />;
+	}
+};
+
+const initialLayout = { width: Dimensions.get('window').width };
+
+export default function index({ navigation }) {
+	const [index, setIndex] = React.useState(0);
+	const [routes] = React.useState([
+		{ key: 'account' },
+		{
+			key: 'stats',
+		},
+		{
+			key: 'settings',
+		},
+	]);
+
+	const renderScene = SceneMap({
+		account: FirstRoute,
+		stats: SecondRoute,
+		settings: AddChildren,
+	});
+
+	return (
+		<TabView
+			navigationState={{ index, routes }}
+			renderScene={renderScene}
+			onIndexChange={setIndex}
+			initialLayout={initialLayout}
+			renderTabBar={(props) => (
+				<TabBar
+					{...props}
+					indicatorStyle={{ backgroundColor: 'red' }}
+					renderIcon={(props) => getTabBarIcon(props)}
+					tabStyle={styles.bubble}
+					labelStyle={styles.noLabel}
+					style={{ backgroundColor: 'black' }}
+				/>
+			)}
+			tabBarPosition='bottom'
+		/>
+	);
+}
+
+const styles = StyleSheet.create({
+	scene: {
+		flex: 1,
+	},
+});
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import Home from './pages/Home';
+import LoadingScreen from './pages/LoadingScreen';
 import Welcome from './pages/Welcome';
 import Login from './pages/Login';
 import ChooseUser from './pages/ChooseUser';
@@ -11,51 +16,100 @@ import SisvanConsent from './pages/Register/SisvanConsent';
 import ParentCPF from './pages/Register/ParentCPF';
 import AddChildren from './pages/AddChildren';
 import BarcodeReader from './pages/BarcodeReader';
+import { useEffect } from 'react';
 
 export default function routes() {
 	const { Navigator, Screen } = createStackNavigator();
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+
+	const readData = async (key) => {
+		try {
+			const value = await AsyncStorage.getItem(key);
+			if (value !== null) {
+				return value;
+			}
+		} catch (e) {
+			console.warn('Error while reading AsyncStorage', err);
+			return null;
+		}
+	};
+
+	useEffect(() => {
+		(async () => {
+			const accessToken = await readData('accessToken');
+			if (accessToken !== null) {
+				setIsAuthenticated(true);
+				setIsLoading(false);
+			} else {
+				setIsLoading(false);
+			}
+		})();
+	}, []);
 
 	return (
 		<NavigationContainer>
-			<Navigator>
-				<Screen
-					name='Welcome'
-					component={Welcome}
-					options={{ headerShown: false }}
-				></Screen>
-				<Screen name='Login' component={Login}></Screen>
-				<Screen
-					name='ChooseUser'
-					component={ChooseUser}
-					options={{ headerShown: false }}
-				></Screen>
-				<Screen
-					name='Register'
-					component={Register}
-					options={{ headerShown: false }}
-				></Screen>
-				<Screen
-					name='ParentDetails'
-					component={ParentDetails}
-					options={{ headerShown: false }}
-				></Screen>
-				<Screen
-					name='SisvanConsent'
-					component={SisvanConsent}
-					options={{ headerShown: false }}
-				></Screen>
-				<Screen
-					name='ParentCPF'
-					component={ParentCPF}
-					options={{ headerShown: false }}
-				></Screen>
-				<Screen
-					name='AddChildren'
-					component={AddChildren}
-					options={{ headerShown: false }}
-				></Screen>
-				<Screen name='BarcodeReader' component={BarcodeReader}></Screen>
-			</Navigator>
+			{isLoading && (
+				<Navigator>
+					<Screen
+						name='LoadingScreen'
+						component={LoadingScreen}
+						options={{ headerShown: false }}
+					></Screen>
+				</Navigator>
+			)}
+
+			{!isAuthenticated && !isLoading && (
+				<Navigator>
+					<Screen
+						name='Welcome'
+						component={Welcome}
+						options={{ headerShown: false }}
+					></Screen>
+					<Screen name='Login' component={Login}></Screen>
+					<Screen
+						name='ChooseUser'
+						component={ChooseUser}
+						options={{ headerShown: false }}
+					></Screen>
+					<Screen
+						name='Register'
+						component={Register}
+						options={{ headerShown: false }}
+					></Screen>
+					<Screen
+						name='ParentDetails'
+						component={ParentDetails}
+						options={{ headerShown: false }}
+					></Screen>
+					<Screen
+						name='SisvanConsent'
+						component={SisvanConsent}
+						options={{ headerShown: false }}
+					></Screen>
+					<Screen
+						name='ParentCPF'
+						component={ParentCPF}
+						options={{ headerShown: false }}
+					></Screen>
+					<Screen
+						name='AddChildren'
+						component={AddChildren}
+						options={{ headerShown: false }}
+					></Screen>
+					<Screen name='BarcodeReader' component={BarcodeReader}></Screen>
+				</Navigator>
+			)}
+
+			{isAuthenticated && !isLoading && (
+				<Navigator>
+					<Screen
+						name='Home'
+						component={Home}
+						options={{ headerShown: false }}
+					></Screen>
+				</Navigator>
+			)}
 		</NavigationContainer>
 	);
 }

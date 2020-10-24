496f87a1d0b22983c9ad5c5eb7440cea12d2d9b0
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-23 22:49:50 -0300

    feat: add basic login screen

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import BarcodeReader from './pages/BarcodeReader';
+import Login from './pages/Login';
 
 export default function routes() {
 	const { Navigator, Screen } = createStackNavigator();
@@ -10,6 +11,7 @@ export default function routes() {
 	return (
 		<NavigationContainer>
 			<Navigator>
+				<Screen name='Login' component={Login}></Screen>
 				<Screen name='BarcodeReader' component={BarcodeReader}></Screen>
 			</Navigator>
 		</NavigationContainer>

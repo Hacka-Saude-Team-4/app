eee2d45cb6d6f0acf95ba0f0e5277384b06da164
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-24 09:14:21 -0300

    feat: add Welcome screen

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import BarcodeReader from './pages/BarcodeReader';
+import Welcome from './pages/Welcome';
 import Login from './pages/Login';
+import BarcodeReader from './pages/BarcodeReader';
 
 export default function routes() {
 	const { Navigator, Screen } = createStackNavigator();
@@ -11,6 +12,7 @@ export default function routes() {
 	return (
 		<NavigationContainer>
 			<Navigator>
+				<Screen name='Welcome' component={Welcome}></Screen>
 				<Screen name='Login' component={Login}></Screen>
 				<Screen name='BarcodeReader' component={BarcodeReader}></Screen>
 			</Navigator>

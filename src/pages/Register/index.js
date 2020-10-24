9c4161dce2b2c9aa5a9c0eee9d1bc0cc18bedd42
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-24 12:22:40 -0300

    feat: add basic layout for registration

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
@@ -8,7 +9,33 @@ export default function index({ route, navigation }) {
 
 	return (
 		<View style={styles.container}>
-			<Text>Registro para: {userType}</Text>
+			<Text>Criar conta como {userType.toUpperCase()}</Text>
+			<View style={styles.inputContainer}>
+				<Text style={styles.inputTitle}>E-mail</Text>
+				<TextInput
+					autoCapitalize='none'
+					textContentType='emailAddress'
+					placeholder='joao@gmail.com'
+					style={styles.textInput}
+				></TextInput>
+			</View>
+
+			<View style={styles.inputContainer}>
+				<Text style={styles.inputTitle}>Senha</Text>
+				<TextInput
+					autoCapitalize='none'
+					placeholder='********'
+					style={styles.textInput}
+					secureTextEntry={true}
+					textContentType='password'
+				></TextInput>
+			</View>
+
+			<TouchableOpacity style={styles.sendBtn}>
+				<View>
+					<Text>Cadastrar-se</Text>
+				</View>
+			</TouchableOpacity>
 		</View>
 	);
 }

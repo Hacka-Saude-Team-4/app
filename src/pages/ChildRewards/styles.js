a7d527ca3cbd02645c9d4f4cd18e4ea59786bfa3
Danieru-San <fabio99alvarenga@gmail.com>
2020-10-25 14:03:45 -0300

    feat: add challenges from backend to ChildHome

--- src/pages/ChildRewards/styles.js
+++ src/pages/ChildRewards/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 export default StyleSheet.create({
 	container: {
@@ -29,5 +29,58 @@ export default StyleSheet.create({
 		flexDirection: 'row',
 		width: 200,
 		justifyContent: 'space-around',
+	},
+
+	flatListArea: {
+		width: Dimensions.get('window').width,
+		height: 400,
+		marginTop: 10,
+	},
+
+	text: {
+		color: 'red',
+	},
+
+	item: {
+		width: Dimensions.get('window').width,
+		borderColor: 'black',
+		borderWidth: 1,
+		padding: 20,
+		flexDirection: 'row',
+		flex: 1,
+		justifyContent: 'space-between',
+	},
+
+	childInfo: {},
+
+	qr: {},
+
+	access: {
+		fontSize: 20,
+		paddingBottom: 30,
+	},
+
+	name: {
+		fontSize: 20,
+		color: 'black',
+	},
+
+	modal: {
+		backgroundColor: 'white',
+		height: 400,
+		borderRadius: 30,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	addChildrenBtn: {
+		position: 'relative',
+		top: 20,
+	},
+
+	eduh: {
+		marginTop: 20,
+		width: 200,
+		textAlign: 'center',
 	},
 });
